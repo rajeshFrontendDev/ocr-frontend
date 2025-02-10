92f3833d619858eb156b7e662e34c205e59d6a51
rajeshFrontendDev <rajesh.choudhary@thinkmatra.io>
2025-02-10 17:20:32 +0530

    first commit

---
+++ src/customHooks/fetchApi.ts
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const fetchApi = (url: string, method: string) => {
+    const [data, setData] = React.useState(null)
+    const [isLoading, setIsLoading] = React.useState(false)
+    const [error, setError] = React.useState<unknown>(null)
+
+    React.useEffect(() => {
+        const callApi = async () => {
+            setIsLoading(true)
+            try {
+                const res = await fetch(url, {
+                    method: method,
+                    headers: {}
+                })
+                if (!res.ok) {
+                    setError('response not ok')
+                }
+                const data = await res.json()
+                setData(data)
+                setIsLoading(false)
+            } catch (error) {
+                setError(error)
+                setIsLoading(false)
+            }
+        }
+
+        callApi()
+    }, [])
+
+    return { data, isLoading, error }
+}
